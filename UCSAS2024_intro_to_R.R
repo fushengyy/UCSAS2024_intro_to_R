5b929687b44dd088febb93d575aec13a8c109fda
fushengyy <97983823+fushengyy@users.noreply.github.com>
2024-04-11 23:10:18 -0400

    update version

--- UCSAS2024_intro_to_R.R
+++ UCSAS2024_intro_to_R.R
@@ -1,13 +1,17 @@
 # R Basics
 
-## R as a Calculator
-3+8
-exp(3)
-sqrt(15)
-
 ## Getting Help
 help(mean)
 ?mean
+
+
+
+## R as a Calculator
+3 + 8
+exp(3)
+sqrt(15)
+
+
 
 ## Assignment Operators
 x <- 5
@@ -17,6 +21,20 @@ y
 z = 2024
 z
 
+x <- 1 + 1
+x
+1 + 1 -> x
+x
+
+1:10
+mean(x = 1:10)
+mean(x <- 1:10)
+
+a = (1 + 2) * 3 / 4
+a ^ 2
+a %% 2                            # x %% y: modulus (x mod y)
+a %/% 2                           # x %/% y: integer division
+
 
 
 # R Data Structure
@@ -24,27 +42,24 @@ z
 ## Data Types
 
 ### Logical
-bool1 <- TRUE # or bool1 <- T
-bool1
-class(bool1)
+is_active <- TRUE
+has_passed <- FALSE
 
-bool2 <- FALSE # or bool2 <- F
-bool2
-class(bool2)
+is_active
+class(is_active)  # Check class
 
 ### Numeric
-weight <- 63.5
-weight
-class(weight)
+weight <- 63.5 # A floating-point number
+height <- 182 # Also stored as numeric by default
 
-height <- 182
 height
 class(height)
 
 ### Integer
-integer_variable <- 186L
-integer_variable
-class(integer_variable)
+count <- 5L  # An integer
+
+count
+class(count)
 
 ### Complex
 complex_value <- 3 + 2i
@@ -52,107 +67,105 @@ complex_value
 class(complex_value)
 
 ### Charactor
-my_char <- 'A'
-my_char
-class(my_char)
+name <- "Alice"
+sentence <- 'Hello, world!'
 
-fruit <- "Apple"
-fruit
-class(fruit)
+name
+class(name)
+
+
 
 ## Data Structures
 
 ### Vector
-X <- c(1.2, 3.5, 9.2, 7.7, 8.1)
-X
-X[2] # check the second element
-X <- c(X, 22.3) # add elements
-X
-typeof(X) # check type
+numeric_vector <- c(1, 2, 3, 4, 5)  # Numeric vector
+integer_vector <- c(1L, 6L, 10L)  # Integer vector (with `L` suffix)
+character_vector <- c("a", "b", "c")  # Character vector
+logical_vector <- c(TRUE, FALSE, TRUE)  # Logical vector
 
-Y <- c(TRUE, TRUE, FALSE, FALSE)
-Y
-typeof(Y)
+numeric_vector
+typeof(numeric_vector)  # Check type
 
-### List
-empId = c(1, 2, 3, 4) # 1st attribute: employee IDs
-empName = c("Debi", "Sandeep", "Subham", "Shiba") # 2nd attribute: employee names
-numberOfEmp = 4 # 3rd attribute: number of employees
-empList = list(empId, empName, numberOfEmp) # make a list
-empList
-typeof(empList)
-empList[[2]] # 2nd element of the list
-empList[[2]][2] # 2nd element of the 2nd element of the list
+x <- 1:3                         # (1, 2, 3)
+length(x)                        # 3
+names(x) <- letters[x]           # add names to x: "a", "b", and "c"
+y <- setNames(x, LETTERS[x])     # (1, 2, 3) with names "A", "B", and "C"
+names(y)                         # "A", "B", and "C"
+y["A"]                           # 1 with name "A"
+head(y, 2)                       # (1, 2) with names "A", "B"
+head(y, - 1)                     # the same with `head(y, 2)`
+y[c("B", "C")] <- c(4, 6)        # y = (1, 4, 6) with names "A", "B", "C"
+rev(y)                           # (4, 2, 1) with names "C", "B", and "A"
+c("A", "B") %in% names(y)        # (TRUE, TRUE)
+z <- rep(letters[x], x)          # z = ("a", "b", "b", "c", "c", "c")
+paste0(z, 1:6)                   # "a1", "b2", "b3", "c4", "c5", and "c6"
 
-### Dataframe
-Name = c("Amiya", "Raj", "Asish")
-Language = c("R", "Python", "Java")
-Age = c(22, 25, 45)
-df = data.frame(Name, Language, Age)
-df
-df[1,3] # Get the element on the first row and the third column
-df[["Age"]] # Get values of the variable "Age"
-df$Age
 
-### Matrix
-A = matrix(
-  c(1, 2, 3, 4, 5, 6, 7, 8, 9), # Taking sequence of elements
-  nrow = 3, ncol = 3, # No of rows and columns
-  byrow = TRUE # By default matrices are in column-wise order                          
-)
-A
-B = matrix(
-  c(1, 2, 3, 4, 5, 6, 7, 8, 9),
-  nrow = 3, ncol = 3
-)
-B
-dim(A) # check dimension of A
+### Matrices and Arrays
+a <- matrix(LETTERS[1:6], nrow = 2)  # 2 x 3 matrix ("A"-"F" by columns)
+dim(a)                               # (2, 3)
+a[2]                                 # "B"
+a[1, ]                               # the first row
+a[, 2]                               # the second column
+a[1, 1:2]                            # "A" and "C"
+a[rbind(c(1, 3), c(2, 1), c(2, 2))]  # "E", "B", "D"
+b <- array(1:12, c(2, 3, 2))         # 2(row) x 3(column) x 2(# of matrices) array
+dim(b)                               # (2, 3, 2)
 
-### Array
-A = array(
-  c(1, 2, 3, 4, 5, 6, 7, 8), # Taking sequence of elements
-  dim = c(2, 2, 2) # Creating two rectangular matrices 
-                   # each with two rows and two columns 
-                   # first 2: row; second 2: column; third 2: number of matrices
-)
-A
-B <- array(1:12, dim = c(2,3,2)) # Creating two rectangular matrices
-                                 # each with two rows and three columns
-B
+
+### Dataframes and Lists
+dat <- data.frame(x = 1:3, y = c("a", "b", "c"))
+dat$z <- as.character(dat$x + 10)
+dat$fac_z <- factor(dat$z)
+str(dat)
+
+x <- list(1:3, "a", c(TRUE, FALSE, TRUE), c(2.3, 5.9))
+names(x) <- c("foo", "bar", "alpha", "beta")
+str(x)
+y <- list(x, sum)
+str(y)
+y[[2]](1:10)
+
+z <- list(list(list()))  # nested list
+str(z)
+
 
 
 ## Function
-my_function <- function() { # create a function with the name my_function
+my_function1 <- function() { # create a function with the name my_function
   print("Hello World!")
 }
-my_function()
+my_function1()
 
-my_function <- function(fname, lname) {
+my_function2 <- function(fname, lname) {
   paste(fname, lname)
 }
-my_function("Peter", "Griffin")
+my_function2("Peter", "Griffin")
 
-my_function <- function(country = "Norway") { # set default argument to 
+my_function3 <- function(country = "Norway") { # set default argument to 
                                               # country = "Norway"
   paste("I am from", country)
 }
-my_function() # will get the default value, which is "Norway"
-my_function("USA") # replace the default value by "USA"
+my_function3() # will get the default value, which is "Norway"
+my_function3("USA") # replace the default value by "USA"
 
-my_function <- function(x) {
+my_function4 <- function(x) {
   return (5 * x)
 }
-my_function(5)
+my_function4(5)
+
+
 
 ## Loop
-for (i in 1:5) {
-  print(i)
+for(i in 1:5) {
+  print(paste("The current value of i is", i))
 }
 
-i=1
-while(i <= 5){
-  print(i)
-  i = i + 1
+count <- 1
+while(count <= 5) {
+  print(paste("Count is", count))
+  count <- count + 1  # Important to modify the condition variable to avoid 
+                      # infinite loop
 }
 
 for (i in 1:5) {
@@ -165,20 +178,22 @@ while(i <= 5){
   print(i)
 }
 
+
+
 ## Condition
-x <- -3 # 5
+x <- -3
 if (x < 0) {
   print("x is a negative number")
 }
 
-x <- -3 # 5
+x <- -3
 if (x < 0) {
   print("x is a negative number")
 } else {
   print ("x is either a positive number or zero")
 }
 
-x <- -3 # 0 5
+x <- -3
 if (x < 0) {
   print("x is a negative number")
 } else if (x == 0) {
@@ -196,9 +211,11 @@ for (i in 1:5) { # combime loop and condition
 }
 
 
+
 ## Install and Load R Packages
 install.packages("Lahman")
 library(Lahman)
+
 
 
 ## Search and Read Datasets in a Package
@@ -206,38 +223,140 @@ data(package = "Lahman")
 data("Batting", package = "Lahman")
 
 
+
 ## Data Manipulation using Base R
-str(Batting) # Check variables.
-names(Batting) # Names of the dataset
-head(Batting) # Display the first few rows of the dataset.
-Batting[, 1:4] # Display the first 4 columns of the dataset.
-Batting[1:4,] # Display the first 4 rows of the dataset.
-Batting$playerID # Display values of the variable "playerID".
+str(Batting)                              # Learn about variables
+names(Batting)                            # Names of the dataset
+head(Batting)                             # Display the dataset's first few rows
+Batting[, 1:4]                            # Display the first 4 columns
+Batting[1:4,]                             # Display the first 4 rows 
+Batting$playerID                          # Display all rows of 'playerID'
 Batting[, c("playerID")]
-Batting[1:10, c("playerID", "yearID")] # Display the first 10 elements of 
-                                       # the variables "playerID" and "yearID".
-Batting$CS_SO <- Batting$CS + Batting$SO # Create a new variable "CS_SO" by 
-                                         # adding up values of variables "CS" 
-                                         # and "SO".
+Batting[1:10, c("playerID", "yearID")]    # Display the first 10 elements of 
+                                          # the variables "playerID" and "yearID"
+Batting$CS_SO <- Batting$CS + Batting$SO  # Create a new variable "CS_SO" by 
+                                          # adding up values of variables "CS" 
+                                          # and "SO".
 head(Batting)
-Batting[, c("CS", "SO", "CS_SO")] # Display only "CS", "SO", and "CS_SO".
+Batting[, c("CS", "SO", "CS_SO")]         # Display only "CS", "SO", and "CS_SO"
+
 
 
 ## Data Manipulation using dplyr
 install.packages("dplyr")
 library(dplyr)
+
 data("Salaries", package = "Lahman")
 head(Salaries)
 str(Salaries)
 
 salaries <- Salaries %>%
-  select(playerID, yearID, teamID, salary) # remove league info from the dataset
+  select(playerID, yearID, teamID, salary)  # subset with no league info
 head(salaries)
 
-batting <- left_join(Batting, salaries, by = c("playerID", "yearID", "teamID"))
-                  # Join salary info and batting info together
+batting <- left_join(Batting, salaries,     # Add salary info and batting data
+                     by = c("playerID", "yearID", "teamID"))         
 head(batting)
 
-batting <- batting %>% arrange(playerID, yearID)
-                  # Rearrange orders of the dataset by multiple variables
+mean(batting$salary, na.rm = TRUE)          # average salary
+
+batting <- batting %>% 
+  arrange(playerID, yearID)                 # Rearrange orders of the dataset
 head(batting)
+
+
+
+## Visualization Using Base R
+
+### Sample data
+x <- 1:10
+y <- x^2
+
+### Create a scatter plot
+plot(x, y, main = "Scatter Plot", xlab = "X-axis", ylab = "Y-axis", 
+     col = "blue", pch = 19)
+
+### Create a line plot
+plot(x, y, type = "l", main = "Line Plot", xlab = "X-axis", ylab = "Y-axis", 
+     col = "red")
+
+### Generate random data
+data <- rnorm(100)
+
+### Create a histogram
+hist(data, main = "Histogram", xlab = "Value", col = "green", border = "white")
+
+### Sample data
+categories <- c("A", "B", "C", "D")
+values <- c(5, 10, 15, 20)
+
+### Create a bar plot
+barplot(values, names.arg = categories, main = "Bar Plot", ylab = "Frequency", 
+        col = "cyan")
+
+### Generate sample data
+set.seed(10)
+group1 <- rnorm(100)
+group2 <- rnorm(100, mean = 5)
+
+### Create a box plot
+boxplot(group1, group2, main = "Box Plot", names = c("Group 1", "Group 2"), 
+        col = "orange")
+
+
+
+## Visualization Using ggplot2 (cont.)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+year_data <- batting %>% filter(yearID == "2005")
+head(year_data)
+
+plot(x = year_data$X2B, y = year_data$HR,     # base R
+     xlab = "Doubles", ylab = "Homeruns",
+     main = "Scatterplot: Homeruns vs. Doubles")
+
+ggplot(year_data, aes(x = X2B, y = HR)) +     # ggplot2
+  geom_point() # Scatterplot of Homeruns vs. Doubles in 2005
+
+ggplot(year_data, aes(x = X2B, y = HR)) +
+  geom_point() +
+  labs(x = "Doubles", y = "Homeruns",         # Change label names and add title
+       title = "Scatterplot: Homeruns vs. Doubles")
+                                             
+
+ggplot(year_data, aes(x = X2B, y = HR, color = lgID)) +
+  geom_point() +
+  labs(x = "Doubles", y = "Homeruns",         # Group by League (lgID)
+       title = "Scatterplot: Homeruns vs. Doubles")
+
+totalHR <- Batting %>%                        # Total home runs by year
+  group_by(yearID) %>%
+  summarise(HomeRuns = sum(as.numeric(HR), na.rm=TRUE),
+            Games = sum(as.numeric(G), na.rm=TRUE))
+head(totalHR)
+
+totalHR %>% filter(yearID <= 1918) %>%        # HR by year, pre-1919
+  ggplot(., aes(x = yearID, y = HomeRuns)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Year", y = "Home runs hit")
+
+totalHR %>% filter(yearID <= 1918) %>%        # Take games into account
+  ggplot(., aes(x = yearID, y = HomeRuns/Games)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Year", y = "Home runs per game played")
+
+ggplot(totalHR, aes(x = yearID, y = HomeRuns)) +    # HR for all years
+  geom_point() +
+  geom_path() +
+  geom_smooth() +
+  labs(x = "Year", y = "Home runs hit")
+
+ggplot(totalHR, aes(x = yearID, y = HomeRuns/Games)) +   # HR per game
+  geom_point() +
+  geom_path() +
+  geom_smooth(se = FALSE) +
+  labs(x = "Year", y = "Home runs per game played")
--- UCSAS2024_intro_to_R_Quiz.R
+++ UCSAS2024_intro_to_R_Quiz.R
@@ -6,6 +6,7 @@ sort(x)
 table(x) 
 
 
+
 # Quiz 2 Solution
 1:5
 x1 <- array(1:5, c(1, 1, 5))
@@ -14,6 +15,8 @@ x2 <- array(1:5, c(1, 5, 1))
 x2
 x3 <- array(1:5, c(5, 1, 1))
 x3
+
+
 
 # Quiz 3 Solution
 A <- matrix(c(1, 2, 3, 4), ncol = 2, byrow = TRUE)
@@ -32,11 +35,15 @@ A %*% B
 solve(A)
 solve(B)
 
+
+
 # Quiz 4 Solution
 my_function <- function(x, y) {
   return(y^x)
 }
 my_function(4,5)
+
+
 
 # Quiz 5 Solution
 x <- -9
@@ -48,15 +55,19 @@ if (x %% 2 == 0) { # '%%' indicates x mod y (“x modulo y”)
   print("not divisible by 2 nor by 3...")
 }
 
+
+
 # Quiz 6 Solution
 library(Lahman)
 data("Batting", package = "Lahman")
 
-Batting[Batting$yearID == "1900", ] # 1900 data
-length(unique(Batting$playerID)) # number of players
-length(unique(Batting$teamID)) # number of teams
-Batting1950 <- Batting[Batting$yearID == "1950", ] # new dataframe for 1950
+Batting[Batting$yearID == "1900", ]                 # 1900 data
+length(unique(Batting$playerID))                    # number of players
+length(unique(Batting$teamID))                      # number of teams
+Batting1950 <- Batting[Batting$yearID == "1950", ]  # new dataframe for 1950
 Batting1950
+
+
 
 # Quiz 7 Solution
 player25 <- Batting %>% filter(BB > 25 & yearID < 1900)
